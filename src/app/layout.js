5d13720364d8f4fc758632f5c8754c357256f17c
이장희 <janghee6960@gmail.com>
2024-08-23 18:37:49 +0900

    Add .gitignore, configuration files, and source code

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "이장희 포트폴리오",
+  description: "next.js를 프레임워크를 통해 제작한 포트폴리오",
+  keywords: ["포트폴리오", "next.js", "Portfolio", "이장희포트폴이오"]
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <link rel="icon" type="image/svg+sml" href="favicon.svg" />
+      <body>{children}</body>
+    </html>
+  );
+}
